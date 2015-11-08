3868bb69234d6c491469d1f35623333acc9712c7
Anjum <anjum.shaikh@cognizant.com>
2015-11-08 18:42:54 +0530

    KK : Header template

--- angular-master/app/js/directives.js
+++ angular-master/app/js/directives.js
@@ -1,24 +1,22 @@
-
-var app = angular.module('myapp', []);
-
-
-
-app.directive('header', function() {
-
-    return {
-        restrict: 'AE',
-        replace: 'true',
-        templateUrl : "/views/header/header.html",
-        link : function(s, e, a) {
-            s.myclass = a.name;
-
-        }
-    };
-});
-app.directive('footer', function() {
-    var directive = {};
-    directive.restrict = 'AE';
-    directive.replace = true;
-    directive.templateUrl = "/views/footer/footer.html";
-    return directive;
-});
+//
+//var app = angular.module('dapp', []);
+//
+//app.directive('header', function() {
+//
+//    return {
+//        restrict: 'AE',
+//        replace: 'true',
+//        templateUrl : "templates/header/header.html",
+//        link : function(s, e, a) {
+//            s.myclass = a.name;
+//
+//        }
+//    };
+//});
+//app.directive('footer', function() {
+//    var directive = {};
+//    directive.restrict = 'AE';
+//    directive.replace = true;
+//    directive.templateUrl = "templates/footer/footer.html";
+//    return directive;
+//});
