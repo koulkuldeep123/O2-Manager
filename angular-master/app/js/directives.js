9e65b6eb316180196f9304647c0004cbf171e838
Anjum <anjum.shaikh@cognizant.com>
2015-11-08 16:33:08 +0530

    KK : Header template

--- angular-master/app/js/directives.js
+++ angular-master/app/js/directives.js
@@ -1,2 +1,24 @@
-angular.module('application.directives', []);
 
+var app = angular.module('myapp', []);
+
+
+
+app.directive('header', function() {
+
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl : "/views/header/header.html",
+        link : function(s, e, a) {
+            s.myclass = a.name;
+
+        }
+    };
+});
+app.directive('footer', function() {
+    var directive = {};
+    directive.restrict = 'AE';
+    directive.replace = true;
+    directive.templateUrl = "/views/footer/footer.html";
+    return directive;
+});
